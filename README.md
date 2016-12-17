f326af2a3e2b0497434a4692ccf0f6f5ab8430e2
Rafael Oshiro <rafael.oshiro@gmail.com>
2016-12-17 17:59:52 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Rugby Squad Creator
+
+## Running Specs
+
+In the terminal, run the following commands:
+```
+$ cd rugby_squad_creator
+$ bundle install
+$ bundle exec rspec spec/
+```
---
+++ spec/squad_creator_spec.rb
@@ -0,0 +1,67 @@
+require_relative '../lib/squad_creator'
+
+describe SquadCreator do
+  describe '#create' do
+    context 'when JSON file is not given' do
+      it 'raises exception' do
+
+      end
+    end
+
+    context 'when JSON file is provided' do
+      it 'does not raise exception' do
+
+      end
+
+      it 'returns squad with correct attributes' do
+
+      end
+
+      it 'squad has no injured players' do
+
+      end
+
+      describe 'valid union team' do
+        it 'has 2 props' do
+
+        end
+
+        it 'has 1 hooker' do
+
+        end
+
+        it 'has 2 locks' do
+
+        end
+
+        it 'has 2 flankers' do
+
+        end
+
+        it 'has 2 number eight' do
+
+        end
+
+        it 'has 1 scrum half' do
+
+        end
+
+        it 'has 1 out half' do
+
+        end
+
+        it 'has 2 centres' do
+
+        end
+
+        it 'has 2 wingers' do
+
+        end
+
+        it 'has 1 full back' do
+
+        end
+      end
+    end
+  end
+end
